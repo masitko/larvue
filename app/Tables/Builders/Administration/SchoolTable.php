ff3933b705d367140e5f3594fe773a4175c9b311
vagrant <vagrant@homestead>
2019-01-23 16:57:44 +0000

    school edit works

--- app/Tables/Builders/Administration/SchoolTable.php
+++ app/Tables/Builders/Administration/SchoolTable.php
@@ -12,7 +12,7 @@ class SchoolTable extends Table
     public function query()
     {
         return School::select(\DB::raw('
-            schools.id as "dtRowId"
+            schools.*, schools.id as "dtRowId"
         '));
     }
 }
