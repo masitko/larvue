22724770720981c14716b7532e15ecbb8cb2897c
vagrant <vagrant@homestead>
2019-01-23 16:33:34 +0000

    create school works

--- app/School.php
+++ app/School.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use LaravelEnso\TrackWho\app\Traits\CreatedBy;
+use LaravelEnso\TrackWho\app\Traits\UpdatedBy;
+use LaravelEnso\AddressesManager\app\Traits\Addressable;
+
+
 class School extends Model
 {
-    //
+  use Addressable, CreatedBy, UpdatedBy;
+
+  protected $guarded = [];
 }
